24654d51e49826b651b903b033d320398f7124d6
Max Perraudin <max.perraudin@gmail.com>
2021-01-03 18:54:44 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# The legend of Zelda : Breath of the Wild Compendium
+
+This is a very simple web app to acces the [Hyrule Compendium API](https://github.com/Hyrule-Compendium-API/Hyrule-Compendium-API). The Hyrule compendium is an encyclopedia of all in-game interactive items. You can see a specific item or all items in a category.
+
+It uses [Streamlit](https://www.streamlit.io/) to quickly create a data app.
+
+## Access the web app
+
+*Soon*
+
+## Try it locally
+
+Install the required dependencies. Mainly streamlit and its own deps. It is advised to use a virtual environment.
+
+`pip install -r requirements.txt`
+
+The run the streamlit app with the following command :
+
+`streamlit run main.py`
---
+++ main.py
@@ -0,0 +1,63 @@
+import numpy
+import pandas as pd
+import requests
+import streamlit as st
+
+
+def pre_process(text: str) -> str:
+    return text.replace(" ", "_").lower()
+
+
+@st.cache
+def get_entry(entry: str):
+    response = requests.get(f"https://botw-compendium.herokuapp.com/api/v1/entry/{entry}")
+    return response.status_code, response.json()
+
+
+@st.cache
+def get_category(cat: str):
+    response = requests.get(f"https://botw-compendium.herokuapp.com/api/v1/category/{cat}")
+    return response.status_code, response.json()
+
+
+st.title("The legend of Zelda : Breath of the wild Compendium")
+
+# ----------------------------------
+st.header("Search by name or ID")
+
+entry = st.text_input(label="Entry", value="Bokoblin")
+
+entry_status_code, entry_content = get_entry(pre_process(entry))
+
+if entry_status_code == 200:
+    st.subheader(entry_content["data"]["name"] + " (id:" + str(entry_content["data"]["id"]) + ")")
+    st.write("**Category** : " + str(entry_content["data"]["category"]))
+    st.write(entry_content["data"]["description"])
+    st.write(pd.DataFrame(entry_content["data"]["drops"], columns=["Drops"]))
+else:
+    st.write("Error, This entry does not exists")
+
+# ------------------------------------------
+st.header("Browse by category")
+
+cat = st.radio("category", ["Treasure", "Monsters", "Equipment", "Materials", "Creatures"]).lower()
+
+cat_status_code, cat_contents = get_category(pre_process(cat))
+
+if cat_status_code == 200:
+    if cat == "creatures":
+        food = pd.DataFrame(cat_contents["data"]["food"])
+        cols = food.columns.tolist()
+        food = food[cols[-1:] + cols[:-1]]
+        non_food = pd.DataFrame(cat_contents["data"]["non-food"])
+        df = food.merge(non_food, how="outer")
+        df = df.replace(numpy.nan, "Not Food")
+        st.write(df)
+    else:
+        df = pd.DataFrame(cat_contents["data"])
+        cols = df.columns.tolist()
+        df = df[cols[-1:] + cols[:-1]]
+        st.write(df)
+
+else:
+    st.write("Error")
